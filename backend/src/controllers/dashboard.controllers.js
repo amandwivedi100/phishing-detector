2320aa82ceb6d08a9a60aec8f56fd7981dbf13ce
Aman Dwivedi <amandwivediofficial1@gmail.com>
2025-07-26 01:46:07 +0530

    removed the comment unneccary console.log()

--- backend/src/controllers/dashboard.controllers.js
+++ backend/src/controllers/dashboard.controllers.js
@@ -5,14 +5,12 @@ import { classifyEmailWithAI } from "../service/ai.service.js";
 
 const scanEmailsController = asyncHandler(async (req, res) => {
   const { payload } = req.body;
-  // console.log("🔥 RECEIVED BODY:", req.body);
 
   if (!payload) {
     throw new ApiError(400, "Missing payload");
   }
 
   const response = await classifyEmailWithAI(payload);
-  console.log("🔥 RESPONSE FROM AI:", response);
   res
     .status(200)
     .json(new ApiResponse(200, response, "Scanned emails successfully."));
